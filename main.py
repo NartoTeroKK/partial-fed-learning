4e245d207f4383e75eabdf294497a5ec7df1ce99
NartoTeroKK <marcoperozzi00@gmail.com>
2023-09-10 17:11:27 +0200

    save results

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import hydra
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
+import pickle
+from pathlib import Path
 import flwr as fl
 
 from dataset import prepare_dataset
@@ -36,8 +39,19 @@ def main(cfg: DictConfig):
         client_fn=client_fn,
         num_clients=cfg.num_clients,
         config=fl.server.ServerConfig(num_rounds=cfg.num_rounds),
-        strategy=strategy
+        strategy=strategy,
+        client_resources={'num_cpus': cfg.num_cpus, 'num_gpus': cfg.num_gpus}
     )
+
+    ## 6. Save your results
+    save_path = HydraConfig.get().runtime.output_dir
+    results_path = Path(save_path) / 'result.pkl'
+
+    results = {'history':history}
+
+    with open(str(results_path), 'wb') as h:
+        pickle.dump(results, h, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 if __name__ == '__main__':
     main()
